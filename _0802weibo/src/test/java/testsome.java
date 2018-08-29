931c82a6c381e7f8d63ecc9a6eb0c611a155a3f6
Adrian <zooyoowoo@gmail.com>
2018-08-29 19:12:24 +0800

    optimise

--- _0802weibo/src/test/java/testsome.java
+++
@@ -1,9 +0,0 @@
-import com.weibo.hbaseInitialize.TableEnum;
-import org.junit.Test;
-
-public class testsome {
-	@Test
-	public void test1() {
-		System.out.println (TableEnum.TABLE_CONTENT.getClass ());
-	}
-}
